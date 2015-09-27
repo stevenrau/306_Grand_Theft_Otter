548b1975ede9c521d562bd90ecd1a9369784d6d9
stevenrau <srau14@gmail.com>
2015-09-26 21:30:53 -0600

    Added den and floor instantiation at game startup
    -> Added more detail to den sprite
    -> Drew a floor sprite
    -> Made den and floor prefabs
    -> Added den and floor generation at the game_setup script

--- Grand_Theft_Otter/Assets/Scripts/game_setup.cs
+++ Grand_Theft_Otter/Assets/Scripts/game_setup.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class game_setup : MonoBehaviour {
 
@@ -18,17 +19,26 @@ public class game_setup : MonoBehaviour {
 		 * ************************************************************************************/
 		GameObject seaweed_left = Instantiate(Resources.Load("Seaweed")) as GameObject;
 		GameObject seaweed_right = Instantiate(Resources.Load("Seaweed")) as GameObject;
-		seaweed_left.transform.Translate(new Vector2 (-0.2f, -0.1f));
+		//seaweed_left.transform.Translate(new Vector2 (-0.2f, -0.1f));
 		Transform left_transform = seaweed_left.transform;
 		seaweed_right.transform.position = new Vector3(Mathf.Abs (left_transform.position.x), left_transform.position.y, left_transform.position.z);
 		seaweed_right.transform.localScale = new Vector2(-seaweed_left.transform.localScale.x, seaweed_left.transform.localScale.y);
+
 
 		/***************************************************************************************
 		 * Load the otter dens
 		 * ************************************************************************************/
 
-		//GameObject otter_den_left = Instantiate(Resources.Load("Den")) as GameObject;
-		//GameObject otter_den_right = Instantiate(Resources.Load("Den")) as GameObject;
+		GameObject otter_den_left = Instantiate(Resources.Load("Den")) as GameObject;
+		GameObject otter_den_right = Instantiate(Resources.Load("Den")) as GameObject;
+		left_transform = otter_den_left.transform;
+		otter_den_right.transform.position = new Vector3(Mathf.Abs (left_transform.position.x), left_transform.position.y, left_transform.position.z);
+		otter_den_right.transform.localScale = new Vector2(-otter_den_left.transform.localScale.x, otter_den_left.transform.localScale.y);
+
+		/***************************************************************************************
+		 * Load the floor
+		 * ************************************************************************************/
+		GameObject floor = Instantiate (Resources.Load ("Floor")) as GameObject;
 	}
 	
 	// Update is called once per frame
