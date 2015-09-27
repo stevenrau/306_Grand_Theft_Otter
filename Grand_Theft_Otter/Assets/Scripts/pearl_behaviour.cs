ecdb88b091cd016421e211e891a32753b8a2019e
stevenrau <srau14@gmail.com>
2015-09-27 01:22:39 -0600

    Added initial pearl behaviour script
    -> Added script to handle pearl behaviour
    -> It intantiates a respawn point that it will use later
    -> The pearl was given a trigger collider that reacts to players
       only.
    -> The pearl beahviour script checks for motion and animated the
       pearl if it is in motion
    -> Added tags for all the prefabs

---
+++ Grand_Theft_Otter/Assets/Scripts/pearl_behaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pearl_behaviour : MonoBehaviour {
+
+	private GameObject spawn_point;  //The pearl spawn point
+	private bool respawn = false;
+	private Transform transform;    //The transform component for the pearl
+
+	private Vector3 prev_pos;      //The pearl's position from last frame (detect motion)
+	private Vector2 cur_pos;       //The pearl's position in the current frame
+
+	Animator animator; //the animator for the pearl
+
+	// Use this for initialization
+	void Start () {
+		//Instantiate the spawn point and set it to the global var
+		spawn_point = Instantiate (Resources.Load ("Pearl_Spawn")) as GameObject;
+
+		transform = GetComponent<Transform> ();
+		animator = GetComponent<Animator>();
+
+		prev_pos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (respawn) {
+			transform.position = spawn_point.transform.position;
+
+			respawn = false;
+		}
+
+		// Check for motion and animate appropriately
+		if ((prev_pos.x != this.transform.position.x) || 
+			(prev_pos.y != this.transform.position.y)) {
+			animator.SetBool ("in_motion", true);
+		} else {
+			animator.SetBool ("in_motion", false);
+		}
+
+		prev_pos = this.transform.position;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			respawn = true;
+		}
+	}
+}
