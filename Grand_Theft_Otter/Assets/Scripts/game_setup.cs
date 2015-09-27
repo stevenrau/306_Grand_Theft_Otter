eae7f996a556e49cd5a6c043c4a6fb8c741d6588
stevenrau <srau14@gmail.com>
2015-09-26 19:20:09 -0600

    Added game setup script
    -> Added a game setup script that will instantiate the game
       world on startup. It should just be tied to an empty game
       object.
    -> Wrote code to load the seaweed barriers.
    -> Moved prefabs from "Prefabs" folder to "Resources" folder so
       that they can be looked up within the scripts
    -> Renamed dam to den in prep for making that a prefab

---
+++ Grand_Theft_Otter/Assets/Scripts/game_setup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game_setup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake()
+	{
+		Debug.Log (Screen.width);
+		Debug.Log (Screen.height);
+
+		/***************************************************************************************
+		 * Load the seaweed barriers
+		 * ************************************************************************************/
+		GameObject seaweed_left = Instantiate(Resources.Load("Seaweed")) as GameObject;
+		GameObject seaweed_right = Instantiate(Resources.Load("Seaweed")) as GameObject;
+		seaweed_left.transform.Translate(new Vector2 (-0.2f, -0.1f));
+		Transform left_transform = seaweed_left.transform;
+		seaweed_right.transform.position = new Vector3(Mathf.Abs (left_transform.position.x), left_transform.position.y, left_transform.position.z);
+		seaweed_right.transform.localScale = new Vector2(-seaweed_left.transform.localScale.x, seaweed_left.transform.localScale.y);
+
+		/***************************************************************************************
+		 * Load the otter dens
+		 * ************************************************************************************/
+
+		//GameObject otter_den_left = Instantiate(Resources.Load("Den")) as GameObject;
+		//GameObject otter_den_right = Instantiate(Resources.Load("Den")) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
