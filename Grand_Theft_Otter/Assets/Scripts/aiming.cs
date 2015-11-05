f72f61c27b87351c7d3676896ff56b433caeaa63
bvh895 <benj.hingston@usask.ca>
2015-11-05 15:22:58 -0600

    Seperating player_mov script
    player aimming and collision detection
    TODO: get_input
    ignoring meta files

---
+++ Grand_Theft_Otter/Assets/Scripts/aiming.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	GameObject inputScript;
+
+	//the child object of the player that will indicate the direction the pearl will be thrown
+	GameObject pearlOffset;
+	//the component that will either show or hide the pearl on the beaver.
+	public SpriteRenderer pearlRenderer;
+
+	float facingAngle; //the angle the beaver is looking( what way its head is pointing)
+	float throwAngle; // the angle the pearl will be thrown
+
+
+	// Use this for initialization
+	void Start () {
+
+		inputScript = transform.GetComponent<get_input> ();
+
+		//get references to the child pearl
+		pearlOffset = transform.GetChild(0).gameObject;
+
+		//get reference to the sprite renderer located on the pearlOffset child object
+		pearlRenderer = pearlOffset.GetComponent<SpriteRenderer>();
+		
+		throwAngle = 0.0f;
+		facingAngle = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//rotatePearlOffset (Input.GetAxis (aim_horz), Input.GetAxis (aim_vert));
+		rotatePearlOffset (Input.GetAxis (inputScript.GetAimHorizontal()), Input.GetAxis (GetAimVertical()));
+	}
+
+	//point the pearl based on right analog stick
+	void rotatePearlOffset(float x, float y)
+	{
+		
+		
+		// cancel all input below this 
+		if (Mathf.Abs(x) < R_analog_threshold) { x = 0.0f; }
+		if (Mathf.Abs(y) < R_analog_threshold) { y = 0.0f; }
+		
+		// CALCULATE ANGLE AND ROTATE
+		if (x != 0.0f || y != 0.0f)
+		{
+			if(inputScript.IsPC()) {
+				throwAngle = ((Mathf.Atan2(y, x) * Mathf.Rad2Deg) *-1 ) - 180;	// for PC
+			} else {
+				throwAngle = ((Mathf.Atan2(y, x) * Mathf.Rad2Deg)-90);			// for MAC
+			}
+			pearlOffset.transform.rotation = Quaternion.AngleAxis(throwAngle, Vector3.forward);
+		}
+	}
+}
---
+++ Grand_Theft_Otter/Assets/Scripts/collision_detection.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	GameObject pearl_offset; //the child object of the player that will indicate the direction the pearl will be thrown
+	public SpriteRenderer pearl_renderer; //the component that will either show or hide the pearl on the beaver.
+
+	private bool has_pearl = false;
+
+
+	// Use this for initialization
+	void Start() {
+		//get references to the child objects
+		pearl_offset = transform.GetChild (0).gameObject;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		print (other.tag);
+		if (other.tag == "Pearl") {
+			
+			//  GameObject pearlOffset = player.transform.GetChild(0).gameObject;
+			//pearlOffset.GetComponent<SpriteRenderer>().enabled = true;
+			pearl_renderer.enabled = true;
+			pearl_offset.GetComponent<SpriteRenderer>().enabled = true;
+			
+			Destroy(other.gameObject);
+			
+			set_has_pearl(true);
+		}
+	}
+
+	public void set_has_pearl(bool pearl)
+	{
+		has_pearl = pearl;
+	}
+	
+	public bool get_has_pearl()
+	{
+		return has_pearl;
+	}
+	
+	public void hide_pearl()
+	{
+		//transform.GetChild (0).gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		pearl_renderer.enabled = false;
+	}
+
+	void MakeInteractable() {
+		transform.FindChild("beaver_pearl_trigger").gameObject.layer = LayerMask.NameToLayer("Interactable");
+	}
+}
