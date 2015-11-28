74c3a36a41288e3951edb019fa2cbed6c14f0271
lendvoyj <johntheleatherman@gmail.com>
2015-11-27 22:58:53 -0600

    Added a "sight" to improve aiming accuracy
    -> an animated sprite that replaces the old offset preal
    -> only shown when input from right stick is present

--- Grand_Theft_Otter/Assets/Scripts/aiming.cs
+++ Grand_Theft_Otter/Assets/Scripts/aiming.cs
@@ -5,31 +5,33 @@ public class aiming : MonoBehaviour {
 
 	//the child object of the player that will indicate the direction the pearl will be thrown
 	GameObject pearlOffset;
+
 	//the component that will either show or hide the pearl on the beaver.
 	SpriteRenderer pearlRenderer;
 
+	//the animator that shows the aiming guide or not
+	Animator anim;
+
 	float throwAngle; // the angle the pearl will be thrown
 
+	//do not detect input smaller than this to aviod unwanted changes when not touching the stick
 	float aimingThreshold;
 
-	// script
+	//the script that listens for input from the right analog stick
 	get_input inputScript;
 	
 
 
 	// Use this for initialization
 	void Start () {
-
 		inputScript = gameObject.GetComponent<get_input>();
-
-		//get references to the child pearl
+	
 		pearlOffset = transform.GetChild(0).gameObject;
 
-		//get reference to the sprite renderer located on the pearlOffset child object
 		pearlRenderer = pearlOffset.GetComponent<SpriteRenderer>();
+		anim = pearlOffset.GetComponent<Animator> ();
 		
 		throwAngle = 0.0f;
-
 		aimingThreshold = 0.20f;
 	}
 
@@ -41,19 +43,32 @@ public class aiming : MonoBehaviour {
 	//point the pearl based on right analog stick
 	void RotatePearlOffset(float x, float y)
 	{
+
+
 		// cancel all input below this 
 		if (Mathf.Abs(x) < aimingThreshold) { x = 0.0f; }
 		if (Mathf.Abs(y) < aimingThreshold) { y = 0.0f; }
 		
-		// CALCULATE ANGLE AND ROTATE
-		if (x != 0.0f || y != 0.0f)
-		{
-			if(inputScript.PlatformIsPC()) {
-				throwAngle = ((Mathf.Atan2(y, x) * Mathf.Rad2Deg) *-1 ) - 180;	// for PC
+		// the player is aiming the pearl
+		if (x != 0.0f || y != 0.0f) {
+
+			//show the aiming guide
+			anim.SetBool ("Aiming", true);
+
+			if (inputScript.PlatformIsPC ()) {
+				throwAngle = ((Mathf.Atan2 (y, x) * Mathf.Rad2Deg) * -1) - 180;	// for PC
 			} else {
-				throwAngle = ((Mathf.Atan2(y, x) * Mathf.Rad2Deg)-90);			// for MAC
+				throwAngle = ((Mathf.Atan2 (y, x) * Mathf.Rad2Deg) - 90);			// for MAC
 			}
-			pearlOffset.transform.rotation = Quaternion.AngleAxis(throwAngle, Vector3.forward);
+
+			//point the aiming guide in the direction 
+			pearlOffset.transform.rotation = Quaternion.AngleAxis (throwAngle, Vector3.forward);
+		} 
+
+		//not aiming, do not show aiming guide on pearl offset
+		else {
+			anim.SetBool ("Aiming", false);
+
 		}
 	}
 
