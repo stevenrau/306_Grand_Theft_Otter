b419c0d0059293d41fb06572d68fba84e1997d7d
bvh895 <benj.hingston@usask.ca>
2015-12-01 12:00:44 -0600

    fixing beavers start up

--- Grand_Theft_Otter/Assets/Scripts/game_setup.cs
+++ Grand_Theft_Otter/Assets/Scripts/game_setup.cs
@@ -207,10 +207,10 @@ public class game_setup : MonoBehaviour {
 				GameObject beaver4 = createBeaver ("4", startPos4, "Beaver_Player_Scuba", constants.team2Color, constants.player4Color);
 			}
 		} else {
-//			if (constants.hasTech) {
-			if (true) {
-				GameObject beaver1 = createBeaver ("1", startPos1, "Beaver_Player_Scuba", constants.team1Color, constants.player1Color);
-				GameObject beaver2 = createBeaver ("2", startPos2, "Beaver_Player_Scuba", constants.team2Color, constants.player2Color);
+			if (constants.hasTech) {
+//			if (true) {
+				GameObject beaver1 = createBeaver ("1", startPos1, "Beaver_Player", constants.team1Color, constants.player1Color);
+				GameObject beaver2 = createBeaver ("2", startPos2, "Beaver_Player", constants.team2Color, constants.player2Color);
 			} else {
 				GameObject beaver1 = createBeaver ("1", startPos1, "Beaver_Player_Scuba", constants.team1Color, constants.player1Color);
 				GameObject beaver2 = createBeaver ("2", startPos2, "Beaver_Player_Scuba", constants.team2Color, constants.player2Color);
