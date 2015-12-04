9b6683d6266d5a53999d88bdbc1eeabd6ece6ba8
bvh895 <benj.hingston@usask.ca>
2015-12-03 20:19:28 -0600

    Breathing indicator icon
    -> have an icon at the bottom of main game that reads breathing input
    -> animates according to the state of the player:
      (out of water, underwater, dead, breathing in or out)

---
+++ Grand_Theft_Otter/Assets/Scripts/icon_indicator.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class icon_indicator : MonoBehaviour {
+
+	SpriteRenderer beaverIcon;
+	Animator iconAnim;
+	GameObject player;
+	string breatheIn;
+	string breatheOut;
+
+	player_state playerStateScript;
+
+
+	// Use this for initialization
+	void Start () {
+		beaverIcon = GetComponent<SpriteRenderer> ();
+		iconAnim = GetComponent<Animator>();
+
+		if (gameObject.name == "breath_indicator1") {
+			playerStateScript = GameObject.Find ("Beaver1").GetComponent<player_state> ();
+			player = GameObject.Find ("Beaver1").gameObject;
+			breatheIn = "i";
+			breatheOut= "o";
+		} else {
+			playerStateScript = GameObject.Find ("Beaver2").GetComponent<player_state> ();
+			player = GameObject.Find ("Beaver2").gameObject;
+			breatheIn = "k";
+			breatheOut= "l";
+		}
+
+//		if (!constants.hasTech) {
+//			beaverIcon.enabled = false;
+//		}
+
+//		iconAnim.SetBool ("isDead", false);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		iconAnim.ResetTrigger ("breathing_in");
+//		iconAnim.ResetTrigger ("breathing_out");
+//		iconAnim.ResetTrigger ("breath_held");
+
+		// above water
+		if (playerStateScript.GetCanBreathe ()) {
+			iconAnim.SetBool ("isDead", false);
+			iconAnim.SetBool ("underwater", false);
+		} else {
+			iconAnim.SetBool ("underwater", true);
+		}
+
+		// underwater and is dead
+		if (!playerStateScript.GetCanBreathe () && playerStateScript.GetIsSuffocating ()) {
+			iconAnim.SetTrigger("is_dead");
+			iconAnim.SetBool("isDead", true);
+		}
+
+		if(!playerStateScript.GetCanBreathe() ){
+			iconAnim.SetTrigger("breath_held");
+		}
+
+		// underwater
+		if (!playerStateScript.GetIsSuffocating ()) {
+
+			if (Input.GetKey (breatheIn)) {
+				iconAnim.SetTrigger ("breathing_in");
+			}
+			
+			if (Input.GetKey (breatheOut)) {
+				iconAnim.SetTrigger ("breathing_out");
+			}
+		}
+
+	}
+}
